3e0f510421fa46842db3bca8f2d3bc908e617038
Wellington <wellington.schu@live.com>
2016-03-07 21:57:48 -0300

    Alteração na estrutura dos prints

--- Trabalho/src/Criptografia/Main.java
+++ Trabalho/src/Criptografia/Main.java
@@ -9,19 +9,23 @@ public class Main {
         Scanner input = new Scanner(System.in);
         TadCifraDeCesar tad = new TadCifraDeCesar();
 
-        System.out.print("Frase : ");
+        System.out.print("Frase: ");
         String palavra = input.nextLine();
-        System.out.print("Chave : ");
+        System.out.print("Chave: ");
         int key = input.nextInt();
         String cifrada = tad.cifrar(palavra,key);
         String descifrada = tad.decifrar(cifrada,key);
 
-        System.out.println("===========================");
-        System.out.println("Palavra   : " + palavra);
-        System.out.println("Chave     : " + key);
-        System.out.println("CIFRADA   : " + cifrada);
-        System.out.println("DECIFRADA : " + descifrada);
-        System.out.println("===========================");
+        System.out.println();
+        System.out.println("Entrdas");
+        System.out.println();
+        System.out.println("Palavra: " + palavra);
+        System.out.println("Chave: " + key);
+        System.out.println("----------------------");
+        System.out.println("Saidas");
+        System.out.println();
+        System.out.println("Encriptada: " + cifrada);
+        System.out.println("Decriptada: " + descifrada);
 
     }
 
