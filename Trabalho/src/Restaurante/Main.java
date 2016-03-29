0d0b9c896d44244e53d245e4c4c7f7b0c95cfb10
Wellington Schütz <wellington.schu@live.com>
2016-03-29 10:47:31 -0300

    Criação da classe main

---
+++ Trabalho/src/Restaurante/Main.java
@@ -0,0 +1,13 @@
+package Restaurante;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
--- Trabalho/src/Restaurante/Restaurante.java
+++ Trabalho/src/Restaurante/Restaurante.java
@@ -1,10 +1,66 @@
 package Restaurante;
 
-/**
- * Created by welli on 29/03/2016.
- */
 public class Restaurante {
 
+    private static class LISTAMESAS {
 
+        public int quantidadeDeMesas;
+        public LISTAMESAS proximo;
+
+    }
+
+    private static class FILAESPERA {
+
+        public FILAESPERA proximo;
+
+    }
+
+    private static class FILABUFFET {
+
+        public FILABUFFET proximo;
+
+    }
+
+    private static class FILACAIXA {
+
+        public FILACAIXA prox;
+
+    }
+
+    private static class PILHAPRATOS {
+
+        public PILHAPRATOS proximo;
+
+    }
+
+    LISTAMESAS inicioListaDeMesas = null;
+    LISTAMESAS fimListaDeMEsas = null;
+    LISTAMESAS anteriorListaDeMesas = null;
+    LISTAMESAS proximoListaDeMesas = null;
+    LISTAMESAS auxiliarListaDeMesas = null;
+
+    FILAESPERA inicioFilaEspera = null;
+    FILAESPERA fimFilaEspera = null;
+    FILAESPERA anteriorFilaEspera = null;
+    FILAESPERA proximoFilaEspera = null;
+    FILAESPERA auxiliarFilaEspera = null;
+
+    FILABUFFET inicioFilaBuffet = null;
+    FILABUFFET fimFilaBuffet = null;
+    FILABUFFET anteriorFilaBuffet = null;
+    FILABUFFET proximoFilaBuffet = null;
+    FILABUFFET auxiliarFilaBuffet = null;
+
+    FILACAIXA inicioFilaCaixa = null;
+    FILACAIXA fimFilaCaixa = null;
+    FILACAIXA anteriorFilaCaixa = null;
+    FILACAIXA proximoFilaCaixa = null;
+    FILACAIXA auxiliarFilaCaixa = null;
+
+    PILHAPRATOS inicioPilhaPratos = null;
+    PILHAPRATOS fimPilhaPratos = null;
+    PILHAPRATOS anteriorPilhaPratos = null;
+    PILHAPRATOS proximoPilhaPratos = null;
+    PILHAPRATOS auxiliarPilhaPratos = null;
 
 }
